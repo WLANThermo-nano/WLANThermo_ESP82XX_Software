73943a2bb1ccc0ca5a51f0e6c808b54093789c62
sochs <s.ochs.mail@web.de>
2017-04-07 21:00:06 +0200

    Update

--- c_server.h
+++ c_server.h
@@ -366,6 +366,7 @@ void server_setup() {
     server.on("/networklist", HTTP_GET, [](AsyncWebServerRequest *request) { 
       handleWifiResult(request, true);
     });
+    
 
     // REQUEST: /deletenetworkstore
     server.on("/deletenetworkstore", HTTP_POST, [](AsyncWebServerRequest *request) { 
@@ -377,7 +378,17 @@ void server_setup() {
       } 
       request->send(200, "text/plain", "0");
     });
-    
+
+    // REQUEST: /deletenetworkstore
+    server.on("/deletenetworkstore", HTTP_GET, [](AsyncWebServerRequest *request) { 
+      if (setconfig(eWIFI,{})) {  
+        #ifdef DEBUG
+          Serial.println("[INFO]\tReset wifi config");
+        #endif
+        request->send(200, "text/plain", "1");
+      } 
+      request->send(200, "text/plain", "0");
+    });
 
     /*  
     // REQUEST: /setnetwork
@@ -470,49 +481,6 @@ void server_setup() {
         Serial.printf("%s", (const char*)data);
         if(index + len == total) Serial.printf("BodyEnd: %u\n", total);
       }
-      
-    });
-
-    server.onNotFound([](AsyncWebServerRequest *request){
-
-      #ifdef DEBUG
-      Serial.printf("NOT_FOUND: ");
-      if(request->method() == HTTP_GET) Serial.printf("GET");
-      else if(request->method() == HTTP_POST) Serial.printf("POST");
-      else if(request->method() == HTTP_DELETE) Serial.printf("DELETE");
-      else if(request->method() == HTTP_PUT)  Serial.printf("PUT");
-      else if(request->method() == HTTP_PATCH)  Serial.printf("PATCH");
-      else if(request->method() == HTTP_HEAD) Serial.printf("HEAD");
-      else if(request->method() == HTTP_OPTIONS)  Serial.printf("OPTIONS");
-      else  Serial.printf("UNKNOWN");
-      Serial.printf(" http://%s%s\n", request->host().c_str(), request->url().c_str());
-
-      if(request->contentLength()){
-        Serial.printf("_CONTENT_TYPE: %s\n", request->contentType().c_str());
-        Serial.printf("_CONTENT_LENGTH: %u\n", request->contentLength());
-      }
-
-      int headers = request->headers();
-      int i;
-      for(i=0;i<headers;i++){
-        AsyncWebHeader* h = request->getHeader(i);
-        Serial.printf("_HEADER[%s]: %s\n", h->name().c_str(), h->value().c_str());
-      }
-
-      int params = request->params();
-      for(i=0;i<params;i++){
-        AsyncWebParameter* p = request->getParam(i);
-        if(p->isFile()){
-          Serial.printf("_FILE[%s]: %s, size: %u\n", p->name().c_str(), p->value().c_str(), p->size());
-        } else if(p->isPost()){
-          Serial.printf("_POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
-        } else {
-          Serial.printf("_GET[%s]: %s\n", p->name().c_str(), p->value().c_str());
-        }
-      }
-      
-      #endif
-      request->send(404);
       
     });
 
