1e7fdebbc0bf997d9631b721f6b5ebef867a2e56
Phantomias2006 <flo.riedl@gmx.at>
2017-04-09 15:42:38 +0200

    WebFrontend - Menü bei klick auf Link ausblenden

--- data/ui.js
+++ data/ui.js
@@ -3,8 +3,15 @@
     var layout   = document.getElementById('layout'),
         menu     = document.getElementById('menu'),
         menuLink = document.getElementById('menuLink'),
+		menuHome = document.getElementById('menuHome'),
+		menuPlot = document.getElementById('menuPlot'),
+		menuWlan = document.getElementById('menuWlan'),
+		menuSystem = document.getElementById('menuSystem'),
+		menuPitmaster = document.getElementById('menuPitmaster'),
+		menuChart = document.getElementById('menuChart'),
+		menuChannel = document.getElementById('menuChannel'),
+		menuAbout = document.getElementById('menuAbout'),
         content  = document.getElementById('main');
-
     function toggleClass(element, className) {
         var classes = element.className.split(/\s+/),
             length = classes.length,
@@ -35,10 +42,35 @@
     menuLink.onclick = function (e) {
         toggleAll(e);
     };
-
+    menuHome.onclick = function (e) {
+        toggleAll(e);
+    };
+    menuPlot.onclick = function (e) {
+        toggleAll(e);
+    };
+    menuWlan.onclick = function (e) {
+        toggleAll(e);
+    };
+    menuSystem.onclick = function (e) {
+        toggleAll(e);
+    };
+    menuPitmaster.onclick = function (e) {
+        toggleAll(e);
+    };
+    menuChart.onclick = function (e) {
+        toggleAll(e);
+    };
+    menuChannel.onclick = function (e) {
+        toggleAll(e);
+    };
+    menuAbout.onclick = function (e) {
+        toggleAll(e);
+    };
+ 
     content.onclick = function(e) {
         if (menu.className.indexOf('active') !== -1) {
             toggleAll(e);
         }
     };
+
 }(this, this.document));
