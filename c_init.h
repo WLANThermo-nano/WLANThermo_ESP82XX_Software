ac85dcbef3be44a7d58f56c5abfeb4d118bdabb7
sochs <s.ochs.mail@web.de>
2017-06-25 11:23:24 +0200

    Versionsänderung

--- c_init.h
+++ c_init.h
@@ -49,6 +49,7 @@ extern "C" uint32_t _SPIFFS_end;        // FIRST ADRESS AFTER FS
 // SETTINGS
 
 // HARDWARE
+#define FIRMWAREVERSION "v0.4.6"
 
 // CHANNELS
 #define CHANNELS 8                     // UPDATE AUF HARDWARE 4.05
