b3eb570897682f6f2eb5b81bfb4c013299b5dfa9
Holger Imbery <holger@imbery.de>
2017-07-08 10:49:15 +0200

    ADD: action on subscribed topics
    First implementation: modify min/max temp on given channels via
    …/set/temp[int]/[limit], where limit is min or max. New value is
    payload of message.
    Update EE after change

--- c_pmqtt.h
+++ c_pmqtt.h
@@ -84,6 +84,64 @@ void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties 
   DPRINTLN(index);
   DPRINTP("[INFO]\t  total: ");
   DPRINTLN(total);
+  String topic_prefix = F("WLanThermo/");
+  topic_prefix += sys.host;
+  topic_prefix += F("/set/");
+  int topic_prefix_length = topic_prefix.length();
+  String topic_short = String(topic);
+  topic_short.remove(0,topic_prefix_length);
+  DPRINTP("[INFO]\t  ActionString: ");
+  DPRINTLN(topic_short);
+  float new_payload = atof(payload);
+  if (topic_short = "temp0/min"){
+     ch[0].min = new_payload;
+  }
+  else if (topic_short = "temp1/min"){
+       ch[1].min = new_payload;  
+  }
+  else if (topic_short = "temp2/min"){
+       ch[2].min = new_payload; 
+  }
+  else if (topic_short = "temp3/min"){
+       ch[3].min = new_payload;  
+  }
+  else if (topic_short = "temp4/min"){
+       ch[4].min = new_payload;  
+  }
+  else if (topic_short = "temp5/min"){
+       ch[5].min = new_payload;  
+  }
+  else if (topic_short = "temp6/min"){
+       ch[6].min = new_payload;
+  }
+  else if (topic_short = "temp7/min"){
+       ch[7].min = new_payload;
+  }
+  else if (topic_short = "temp0/max"){
+       ch[0].max = new_payload;
+  }
+  else if (topic_short = "temp1/max"){
+       ch[1].max = new_payload;
+  }
+  else if (topic_short = "temp2/max"){
+       ch[2].max = new_payload;
+  }
+  else if (topic_short = "temp3/max"){
+       ch[3].max = new_payload;
+  }
+  else if (topic_short = "temp4/max"){
+       ch[4].max = new_payload;
+  }
+  else if (topic_short = "temp5/max"){
+       ch[5].max = new_payload;
+  }
+  else if (topic_short = "temp6/max"){
+       ch[6].max = new_payload;
+  }
+  else if (topic_short = "temp7/max"){
+       ch[7].max = new_payload;
+  }
+  setconfig(eCHANNEL,{});  
 }
 
 void set_pmqtt() {
@@ -133,7 +191,4 @@ void sendpmqtt() {
       pmqttClient.connect();
     }
 }
-
-
-
 
