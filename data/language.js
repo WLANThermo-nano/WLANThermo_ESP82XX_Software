44139c029bcbbb4460a5312a4add68cd762a90c7
Phantomias2006 <flo.riedl@gmx.at>
2019-03-24 22:58:57 +0100

    translation - initial commit

---
+++ data/language.js
@@ -0,0 +1,49 @@
+	var en = {
+				// Reiter System-Einstellungen
+				system_settings:"System-Einstellungen",
+				unit:"Unit",
+				german:"German",
+				english:"English",
+				fahrenheit:"Fahrenheit",
+				celsius:"Celsius",
+				hostname:"Hostname",
+				ap_name:"AP-Name",
+				hw_version:"Hardwareversion",
+				update_search:"Automatisch nach Updates suchen",
+				// Buttons
+				back:"Back",
+				save:"Save",
+				//Kanaleinstellungen
+				channel_name:"Kanalname",
+				temp_max:"Temp Max",
+				temp_min:"Temp Min",
+				temp_sensor:"Sensor",
+				color:"Color",
+				push_alarm:"Push Alarm",
+				buzzer_alarm:"Summer Alarm"
+			};
+	var de = {
+				// Reiter System-Einstellungen
+				system_settings:"System-Einstellungen",
+				unit:"Einheit",
+				german:"Deutsch",
+				english:"Englisch",
+				fahrenheit:"Fahrenheit",
+				celsius:"Celsius",
+				hostname:"Hostname",
+				hostname:"Hostname",
+				ap_name:"AP-Name",
+				hw_version:"Hardwareversion",
+				update_search:"Automatisch nach Updates suchen",
+				// Buttons
+				back:"Zurück",
+				save:"Speichern",
+				//Kanaleinstellungen
+				channel_name:"Kanalname",
+				temp_max:"Temp Max",
+				temp_min:"Temp Min",
+				temp_sensor:"Fühler",
+				color:"Farbe",
+				push_alarm:"Push Alarm",
+				buzzer_alarm:"Summer Alarm"
+			};
